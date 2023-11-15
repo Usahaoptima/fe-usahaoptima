8c1e30843a636e84c6be0a9133c856ec24ddf4ff
Helvizar <helvizaranderson@gmail.com>
2023-11-16 05:06:33 +0700

    feat: added education page

--- src/pages/edukasi.jsx
+++ src/pages/edukasi.jsx
@@ -1,5 +1,22 @@
-const edukasi = () => {
-  return <div>edukasi</div>;
+import Header from "../components/Layouts/Dashboard/Header";
+import Sidebar from "../components/Layouts/Dashboard/Sidebar";
+import Footer from "../components/Layouts/Dashboard/Footer";
+import EducationContent from "../components/Layouts/Education/Education-Content";
+
+const EducationPage = () => {
+  return (
+    <>
+      <main>
+        <Sidebar />
+        <section id="dashboard">
+          <Header />
+          <EducationContent />
+          <hr className="mt-5" />
+          <Footer />
+        </section>
+      </main>
+    </>
+  );
 };
 
-export default edukasi;
+export default EducationPage;
