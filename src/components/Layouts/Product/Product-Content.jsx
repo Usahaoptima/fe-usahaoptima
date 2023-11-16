4f25a8c23ac3a1800fed4a4b22bd2696a86100b9
Fajrull <fajrulm05@gmail.com>
2023-11-16 18:49:17 +0700

    fix: fix bug commit

---
+++ src/components/Layouts/Product/Product-Content.jsx
@@ -0,0 +1,46 @@
+import Loader from "../../Elements/Loader";
+import TableProduct from "../../Fragments/Product/Table-Product";
+
+const ProdukContent = () => {
+  return (
+    <>
+      <section id="produk" className="mt-4">
+        <div className="produk-button">
+          <button>Tambah Produk</button>
+        </div>
+        <Loader />
+        <div className="section-content">
+          <table className="table table-hover mt-4">
+            <thead>
+              <tr>
+                <TableProduct tableName="#" />
+                <TableProduct tableName="Nama Produk" />
+                <TableProduct tableName="Harga" />
+                <TableProduct tableName="Stok Produk" />
+                <TableProduct tableName="Action" />
+              </tr>
+            </thead>
+            <tbody id="product"></tbody>
+          </table>
+
+          <nav aria-label="Page navigation example">
+            <ul className="pagination justify-content-end gap-3 m-3">
+              <li id="previous" className="page-item">
+                <a className="page-link" href="#">
+                  Previous
+                </a>
+              </li>
+              <li id="next" className="page-item">
+                <a className="page-link" href="#">
+                  Next
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ProdukContent;
