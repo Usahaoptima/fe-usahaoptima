c970ed7c75d002466bdc21b305480e799f19d123
Wutdehel <elizanlilongongopan@gmail.com>
2023-11-16 17:00:16 +0700

    feat: adding footer section for landing page '

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -2,6 +2,7 @@ import NavbarLandingPage from "../components/Layouts/Landing-Page/Navbar-Landing
 import HomeSection from "../components/Layouts/Landing-Page/Home-Section";
 import AboutSection from "../components/Layouts/Landing-Page/About-Section";
 import ServicesSection from "../components/Layouts/Landing-Page/Services-Section";
+import Footer from "../components/Layouts/Landing-Page/Footer";
 import "../assets/css/LandingPage.css";
 
 function landingpage() {
@@ -11,6 +12,7 @@ function landingpage() {
       <HomeSection />
       <AboutSection />
       <ServicesSection />
+      <Footer />
     </>
   );
 }
