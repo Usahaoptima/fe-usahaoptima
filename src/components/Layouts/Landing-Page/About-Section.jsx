cfa25bb1f5d602f5c99a90edd19b855b8a012b96
Wutdehel <elizanlilongongopan@gmail.com>
2023-11-16 14:46:54 +0700

    feat: adding about section

--- src/components/Layouts/Landing-Page/About-Section.jsx
+++ src/components/Layouts/Landing-Page/About-Section.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function AboutSection() {
-  return <div>AboutSection</div>;
+  return (
+    <div class="container-fluid px-0" id="about">
+      <div class="row">
+        <div class="col-md-6 col-sm-6">
+          <img
+            src="../src/assets/img/landing-page/image-content2.svg"
+            alt="image"
+            class="img-fluid"
+          />
+        </div>
+        <div class="col-md-6 col-sm-6 d-flex align-content-center flex-wrap text-center">
+          <div class="content-right">
+            <p class="title-first">ANYTIME</p>
+            <p class="title-second">ANYWHERE</p>
+            <p class="paragraf">
+              Temukan solusi terbaik untuk pengaturan dan optimalisasi keuangan
+              pribadi atau bisnis Anda. Kami menyediakan panduan, tips, dan
+              informasi terbaru untuk membuat keputusan keuangan yang bijak.
+              Bergabunglah dengan komunitas kami dan mulai mengelola keuangan
+              Anda dengan lebih baik.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default AboutSection;
--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,12 +1,14 @@
 import NavbarLandingPage from "../components/Layouts/Landing-Page/Navbar-Landing-Page";
-import HomePage from "../components/Layouts/Landing-Page/Home-Page";
+import HomeSection from "../components/Layouts/Landing-Page/Home-Section";
+import AboutSection from "../components/Layouts/Landing-Page/About-Section";
 import "../assets/css/LandingPage.css";
 
 function landingpage() {
   return (
     <>
       <NavbarLandingPage />
-      <HomePage />
+      <HomeSection />
+      <AboutSection />
     </>
   );
 }
