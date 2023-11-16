c05ca24a5031f63010a69d1326321fc8353e5f58
Wutdehel <elizanlilongongopan@gmail.com>
2023-11-16 14:17:50 +0700

    feat: adding home section

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,10 +1,12 @@
 import NavbarLandingPage from "../components/Layouts/Landing-Page/Navbar-Landing-Page";
+import HomePage from "../components/Layouts/Landing-Page/Home-Page";
 import "../assets/css/LandingPage.css";
 
 function landingpage() {
   return (
     <>
       <NavbarLandingPage />
+      <HomePage />
     </>
   );
 }
