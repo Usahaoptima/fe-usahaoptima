fe86e6d658868249e979a90441b92e8697afac45
Wutdehel <elizanlilongongopan@gmail.com>
2023-11-17 11:42:19 +0700

    chore: solving conflict

--- src/main.jsx
+++ src/main.jsx
@@ -7,31 +7,13 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "../public/assets/css/Dashboard.css";
 import "../public/assets/css/ProdukPage.css";
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "./pages/dashboard";
-import Produk from "./pages/produk";
-import Penjualan from "./pages/penjualan";
-import Edukasi from "./pages/edukasi";
-import BiayaOperasional from "./pages/biaya-operasional";
-import Laporan from "./pages/laporan";
-import Pengaturan from "./pages/pengaturan";
-import LandingPage from "./pages/landing-page";
-import RegisterPage from "./pages/register-page";
+import { BrowserRouter } from "react-router-dom";
+import AppRouter from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/produk" element={<Produk />} />
-        <Route path="/penjualan" element={<Penjualan />} />
-        <Route path="/edukasi" element={<Edukasi />} />
-        <Route path="/biaya-operasional" element={<BiayaOperasional />} />
-        <Route path="/laporan" element={<Laporan />} />
-        <Route path="/pengaturan" element={<Pengaturan />} />
-      </Routes>
+      <AppRouter />
     </BrowserRouter>
   </React.StrictMode>
 );
