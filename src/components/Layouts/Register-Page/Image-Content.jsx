1220edee78be05489beb5452a45055f49f2ed49c
Wutdehel <elizanlilongongopan@gmail.com>
2023-11-17 08:14:33 +0700

    feat: adding content to register-page

---
+++ src/components/Layouts/Register-Page/Image-Content.jsx
@@ -0,0 +1,19 @@
+import ContentRegisterPage from "../../Fragments/Register/Content-Register-Page";
+
+function ImageContent() {
+  return (
+    <ContentRegisterPage>
+      <div class="col-md-6 col-md-6 d-flex align-items-center">
+        <div class="text-wrapper">
+          <h2>Daftar ke</h2>
+          <p>USAHAOPTIMA</p>
+          <span class="mt-3">
+            Sudah punya akun?<a href="index.html"> Login</a>
+          </span>
+        </div>
+      </div>
+    </ContentRegisterPage>
+  );
+}
+
+export default ImageContent;
--- src/pages/register-page.jsx
+++ src/pages/register-page.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HeaderRegisterPage from "../components/Layouts/Register-Page/Header-Register-Page";
+import ImageContent from "../components/Layouts/Register-Page/Image-Content";
 import "../../public/assets/css/RegisterPage.css";
 
 function registerpage() {
   return (
     <>
       <HeaderRegisterPage />
+      <ImageContent />
     </>
   );
 }
