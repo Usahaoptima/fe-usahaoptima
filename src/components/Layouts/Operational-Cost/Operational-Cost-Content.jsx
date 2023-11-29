c76d8273ac3130a3cedc9c6b910eb5875bf3a963
Helvizar <helvizaranderson@gmail.com>
2023-11-29 21:26:16 +0700

    feat: added overview operational cost page

---
+++ src/components/Layouts/Operational-Cost/Operational-Cost-Content.jsx
@@ -0,0 +1,11 @@
+import CostSummary from "../../Fragments/Operational-Cost/Cost-Summary";
+
+const OperationalCostPage = () => {
+  return (
+    <div>
+      <CostSummary />
+    </div>
+  );
+};
+
+export default OperationalCostPage;
